db84494100a9898f11f5932ad98abeea8f6e74ce
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-08 20:29:30 +0530

    commit

--- 03-vars.sh
+++ 03-vars.sh
@@ -6,3 +6,8 @@ user=centos
 echo $a $b $c
 ls -ltr /home/$USER
 ls -ltr /home/${USER}
+
+DATE=2022-09-08
+echo "Good Morning, today is $DATE"
+DATE_DYNAMIC=${date +F}
+echo "Good Morning, today is $DATE_DYNAMIC, a dynamic print"
