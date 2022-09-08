2d1a75fe88463782f34bd4c4612c9e9743b6f8ee
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-08 22:46:15 +0530

    commit

--- common.sh
+++ common.sh
@@ -2,7 +2,7 @@
 
 common_fun()
 {
-    echo "==== Common Function Starts ===="
+    echo -e "\e[32m==== Common Function Starts ====\e[0m"
     echo "This is common function"
     echo "This is called from common.sh"
     echo "==== Common Function Ends ===="
