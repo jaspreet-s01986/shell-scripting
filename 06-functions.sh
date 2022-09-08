65b98bbd0d336868f9c1fdad51bf232463d7cee9
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-08 22:20:10 +0530

    commit

--- 06-functions.sh
+++ 06-functions.sh
@@ -17,3 +17,13 @@ echo $(uptime | awk -F : '{print $1}') #prints 1st value
 echo $(uptime | awk -F : '{print $4}') #prints 5th value
 echo $(uptime | awk -F : '{print $NF}') #prints last value
 echo $(uptime | awk -F : '{print $NF}' | awk -F , '{print $1}') #prints first value from last column
+
+stat ()
+{
+    echo "Load Average from last 1 minute is $(uptime | awk -F : '{print $NF}' | awk -F , '{print $1}')"
+    echo "No of open sessions are $(who | wc -l)"
+    echo "Time is $(date +%F) $(date +%T)"
+}
+
+echo "Calling stat function"
+stat
