da277cb34673c166b2f9b2e000812f3cb1040b68
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-08 23:02:49 +0530

    commit

--- 07-exit.sh
+++ 07-exit.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 #Exit code 0 to 255, developers are advised to use 0-125, other are used by system
+# 0 is for success, rest are for errors, warnings etc
 
 echo Hello
-exit 0
+#exit 0
+exit 57
 echo "learning DevOps"
