7d738cf22c397583e99d7909c6fd50fa12022a78
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-08 20:32:45 +0530

    commit

--- 03-vars.sh
+++ 03-vars.sh
@@ -9,5 +9,5 @@ ls -ltr /home/${USER}
 
 DATE=2022-09-08
 echo "Good Morning, today is $DATE"
-DATE_DYNAMIC=${date}
+DATE_DYNAMIC=$(date +%F)
 echo "Good Morning, today is $DATE_DYNAMIC, a dynamic print"
