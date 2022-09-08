f702a9d5e9fdd23a33e97c1444dd0b4a474601df
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-08 22:07:46 +0530

    commit

--- 06-functions.sh
+++ 06-functions.sh
@@ -2,7 +2,7 @@
 
 fa()
 {
-echo "Today is $(date +%F date +%T)"
+echo "Today is $(date +%T%F)"
 echo "No of open sessions are $(who | wc -l)"
 echo "Function fa completed"
 }
@@ -11,3 +11,8 @@ echo "Calling function"
 fa
 sleep 5
 fa
+
+echo $(uptime)
+echo $(uptime | awk -F : $1) #prints 1st value
+echo $(uptime | awk -F : $5) #prints 5th value
+echo $(uptime | awk -F : $NF) #prints last value
