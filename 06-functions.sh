c128a51ff179b4497a0dd322acb03b2ece78e72c
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-08 22:11:52 +0530

    commit

--- 06-functions.sh
+++ 06-functions.sh
@@ -13,6 +13,6 @@ sleep 5
 fa
 
 echo $(uptime)
-echo $(uptime | awk -F : '{print $1}) #prints 1st value
-echo $(uptime | awk -F : '{print $5}) #prints 5th value
-echo $(uptime | awk -F : '{print $NF}) #prints last value
+echo $(uptime | awk -F : '{print $1}') #prints 1st value
+echo $(uptime | awk -F : '{print $5}') #prints 5th value
+echo $(uptime | awk -F : '{print $NF}') #prints last value
