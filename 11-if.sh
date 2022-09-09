a4456de4b269a2fe74d542fece4383273b0753a7
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-09 19:29:43 +0530

    commit

--- 11-if.sh
+++ 11-if.sh
@@ -3,4 +3,8 @@ ACTION=$1
 
 if [ "$ACTION" = "start" ]; then
     echo -e "Selected option is \e[34m start \e[0m"
+elif [ "$ACTION" = "stop" ]; then
+    echo -e "Selected option is \e[34m start \e[0m"
+else
+    echo -e "Valid options are \e[34m start \e[0m \e[35m stop \e[0m"
 fi
