b9aa84845fab0f61cbfe677306d0ca5398dd729c
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-09 22:08:06 +0530

    commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e # makes script to exist if any command fails rather than moving to next step
-yum install nginxdfafa -y
+
+USER_ID=$(id -u)
+
+if [ "$USER_ID" -ne 0]; then
+    echo -e "\e[32mYOu need to run the script as root user\e[0m"
+    exit 1
+fi
+yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
