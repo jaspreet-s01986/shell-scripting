79d4fe8f77b4f7dff4f3ab916ccc92ffb2b747e8
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-09 21:35:16 +0530

    commit

--- 11-if.sh
+++ 11-if.sh
@@ -3,7 +3,7 @@ ACTION=$1
 
 if [ "$ACTION" = "start" ]; then
     echo -e "Selected option is \e[34m start \e[0m"
-elif [ "$ACTION" = "stop" ]; then
+elif [ "$ACTION" = "stop" ]; then               #multiple else if (elif) can be used
     echo -e "Selected option is \e[34m start \e[0m"
 else
     echo -e "Valid options are \e[34m start \e[0m \e[35m stop \e[0m"
