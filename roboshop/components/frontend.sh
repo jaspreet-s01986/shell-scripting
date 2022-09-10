8a465a1de59fdf65a87482a894520eb352d6eb47
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-10 07:49:31 +0530

    commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,35 +6,31 @@ LOGFILE="/tmp/$COMPONENT.log"
 
 source components/common.sh
 
+status () {
+    if [ $? -eq 0 ]; then
+        echo -e "\e[32mSUCCESS\e[0m"
+    else
+        echo -e "\e[31mFAILED\e[0m"
+    fi
+}
+
 echo -n "Installing Nginx: "
 yum install nginx -y &>> $LOGFILE
-
-if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-else
-    echo -e "\e[31mFAILED\e[0m"
-fi
+status $?
 
 systemctl enable nginx &>> $LOGFILE
 echo -n "Installing Nginx: "
 systemctl start nginx &>> $LOGFILE
 
-if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-else
-    echo -e "\e[31mFAILED\e[0m"
-fi
+status $?
 
 echo -n "Downloading and extracting $COMPONENT: "
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
-if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-else
-    echo -e "\e[31mFAILED\e[0m"
-fi
+
+status $?
 
 mv frontend-main/* .
 mv static/* .
