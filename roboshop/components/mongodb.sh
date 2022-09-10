94bb68799b7661dd1db56b7fcedf9df3f3f272fd
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-10 14:38:13 +0530

    commit

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -6,6 +6,7 @@ set -e # makes script to exist if any command fails rather than moving to next s
 COMPONENT=mongodb
 LOGFILE="/tmp/$COMPONENT.log"
 MONGODB_REPO_URL="https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo"
+COMPONENT_REPO="https://github.com/stans-robot-project/mongodb/archive/main.zip"
 source components/common.sh
 
 echo -n "Downloading the $COMPONENT package: "
@@ -23,11 +24,18 @@ echo -n "Starting $COMPONENT: "
 systemctl enable mongod &>> $LOGFILE
 systemctl start mongod &>> $LOGFILE
 status $?
-# vim /etc/mongod.conf
-# systemctl restart mongod
-# curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
-# cd /tmp
-# unzip mongodb.zip
-# cd mongodb-main
-# mongo < catalogue.js
-# mongo < users.js
+
+echo -n "Downloading the $COMPONENT code: "
+curl -s -L -o /tmp/mongodb.zip "$COMPONENT_REPO"
+status $?
+cd /tmp
+
+echo -n "Extracting the schema: "
+unzip mongodb.zip &>> $LOGFILE
+status $?
+cd mongodb-main
+echo -n "Injecting the schema: "
+mongo < catalogue.js
+mongo < users.js
+status $?
+echo -e "\e[32m -------- $COMPONENT Configured Successfully --------\e[0m"
