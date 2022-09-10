dd7e89be01731686af47bc2a0c2645bc765d4cf4
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-10 07:31:26 +0530

    commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -10,17 +10,32 @@ echo -n "Installing Nginx: "
 yum install nginx -y &>> $LOGFILE
 
 if [ $? -eq 0 ]; then
-    echo -e "\e[31mSUCCESS\e[0m"
+    echo -e "\e[32mSUCCESS\e[0m"
 else
     echo -e "\e[31mFAILED\e[0m"
 fi
 
-systemctl enable nginx
-systemctl start nginx
+systemctl enable nginx &>> $LOGFILE
+echo -n "Installing Nginx: "
+systemctl start nginx &>> $LOGFILE
+
+if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+else
+    echo -e "\e[31mFAILED\e[0m"
+fi
+
+echo -n "Downloading and extracting $COMPONENT: "
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
+if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+else
+    echo -e "\e[31mFAILED\e[0m"
+fi
+
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
