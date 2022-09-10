ffbd2fd72d3dd82032229653f4249eec12d3b97a
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-10 07:56:02 +0530

    commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -24,15 +24,23 @@ systemctl start nginx &>> $LOGFILE
 
 status $?
 
-echo -n "Downloading and extracting $COMPONENT: "
+echo -n "Downloading $COMPONENT package: "
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip &>> $LOGFILE
 
 status $?
 
+echo -n "Clearing Old Content"
+cd /usr/share/nginx/html
+rm -rf *
+status $?
+echo -n "Extracting $COMPONENT package"
+unzip /tmp/frontend.zip &>> $LOGFILE
+
+status $?
+echo -n "Moving package and updating Porxy file"
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+status $?
