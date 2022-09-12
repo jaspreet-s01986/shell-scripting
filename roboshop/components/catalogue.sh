497f0579b9d57003ddb00b8e79d95dbc4094d071
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-12 08:23:49 +0530

    commit

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 set -e # makes script to exist if any command fails rather than moving to next step
 
-COMPONENT=frontend
+COMPONENT=catalogue
 LOGFILE="/tmp/$COMPONENT.log"
 NODEJS_REPO="https://rpm.nodesource.com/setup_lts.x"
 NODEJS_CODE="https://github.com/stans-robot-project/catalogue/archive/main.zip"
 source components/common.sh
 
-echo -n "Downloading $COMPONENT code: "
+echo -n "Downloading NodeJS code: "
 curl -sL $NODEJS_REPO | bash &> $LOGFILE
 status $?
 echo -n "Installing NodeJS: "
 yum install nodejs -y &>> $LOGFILE
 status $?
 echo -n "Creating roboshop user: "
-useradd roboshop &>> $LOGFILE
+id roboshot &>> $LOGFILE || useradd roboshop &>> $LOGFILE
 status $?
-# echo -n "Downloading NodeJS code: "
-# curl -s -L -o /tmp/catalogue.zip $NODEJS_CODE
-# status $?
-# echo -n "Extracting NodeJS code: "
-# cd /home/roboshop
-# unzip -o /tmp/catalogue.zip
-# status $?
-# mv catalogue-main catalogue
-# cd /home/roboshop/catalogue
-# npm install
+
+echo -n "Downloading $COMPONENT Repo: "
+curl -s -L -o /tmp/$COMPONENT.zip $NODEJS_CODE
+status $?
+echo -n "Extracting NodeJS code: "
+cd /home/roboshop
+unzip -o /tmp/$COMPONENT.zip
+status $?
+mv catalogue-main $COMPONENT
+cd /home/roboshop/$COMPONENT
+echo -n "Installing NPM : "
+npm install
+status $?
 # vim systemd.servce
-# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# mv /home/roboshop/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
 # systemctl daemon-reload
 # systemctl start catalogue
 # systemctl enable catalogue
