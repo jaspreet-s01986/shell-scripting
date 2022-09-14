ed2fbc4e1781850fdf8298ca13ef98b48ac549fa
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-14 21:53:18 +0530

    commit

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e # makes script to exist if any command fails rather than moving to next step
 
-COMPONENT=catalogue
+COMPONENT=user
 LOGFILE="/tmp/$COMPONENT.log"
 source components/common.sh
 
