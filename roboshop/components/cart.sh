76bc7dc6f82560f06325de94cbd41495467d9932
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-15 22:14:22 +0530

    commit

--- roboshop/components/cart.sh
+++ roboshop/components/cart.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -e # makes script to exist if any command fails rather than moving to next step
 
+COMPONENT=cart
+LOGFILE="/tmp/$COMPONENT.log"
 source components/common.sh
-echo "Cart"
 
-
+nodejs
 
 
 echo -e "\e[32m -------- $COMPONENT Configured Successfully --------\e[0m"
