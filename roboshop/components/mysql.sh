5dc9db439308c1fbdf86ae9421708c807897d117
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-15 23:13:37 +0530

    commit

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -20,12 +20,22 @@ systemctl start mysqld
 status $?
 
 #Password needs to change only for the first time
-echo "show databases" | mysql -uroot -pRoboShop@1
+echo "show databases" | mysql -uroot -pRoboShop@1 &>> $LOGFILE
 if [ 0 -ne $? ]; then
     echo -n "Changing Default $COMPONENR Root Password: "
     DEF_ROOT_PASSWD=$(grep "temporary password" /var/log/mysqld.log | awk '{print $NF}')
     echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" > /tmp/rootpassword_change.sql
     mysql --connect-expired-password -uroot -p"$DEF_ROOT_PASSWD" < /tmp/rootpassword_change.sql
+    status $?
+fi
+
+
+#Uninstall validate password plugin
+echo "show plugins" | mysql -uroot -pRoboShop@1 &>> $LOGFILE | grep validate_password
+if [ $? -eq 0 ]; then
+    echo -n "Unistalling $COMPONENR validate password plugin: "
+    echo "uninstall plugin validate_password;" > /tmp/uninstall_pw_validate.sql
+    mysql --connect-expired-password -uroot -pRoboShop@1 < /tmp/rootuninstall_pw_validate.sql
     status $?
 fi
 # grep temp /var/log/mysqld.log
