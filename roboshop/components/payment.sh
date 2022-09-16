a2cb4c2483d4302385152d83436fb2145eb5bcb7
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-16 22:22:42 +0530

    commit

--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -13,21 +13,17 @@ status $?
 create_user
 #Calling Download & Extract function
 download_extract
-# echo -n "Downloading $COMPONENT package: "
-# cd /home/roboshop
-# curl -L -s -o /tmp/payment.zip "https://github.com/stans-robot-project/payment/archive/main.zip"
-# status $?
-# echo -n "Cleaning up: "
-# cd /home/$APPUSER && rm -rf $COMPONENT &>> $LOGFILE
-# status $?
-# echo -n "Extracting $COMPONET Package: "
-# unzip -o /tmp/payment.zip &>> $LOGFILE
-# mv payment-main payment
-# status $?
 
 echo -n "Installing Dependencies: "
 cd /home/roboshop/payment 
 pip3 install -r requirements.txt &>> $LOGFILE
 status $?
+
+echo -n "Updating the App Config $COMPONENT: "
+USER_ID=$(id -u roboshop)
+GROUP_ID=$(id -g roboshop)
+sed -i -e "/uid/ c uid = $USER_ID" "/gid/ c gid = $GROUP_ID" 
+status $?
+
 
 echo -e "\e[32m -------- $COMPONENT Configured Successfully --------\e[0m"
