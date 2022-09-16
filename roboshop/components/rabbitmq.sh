8185c2a1aa19acfe6345c16969cb1592fcb6e4ba
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-16 21:17:19 +0530

    commit

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -4,12 +4,12 @@ set -e # makes script to exist if any command fails rather than moving to next s
 COMPONENT=rabbitmq
 LOGFILE="/tmp/$COMPONENT.log"
 source components/common.sh
-#rpm -ql erlang-23.2.6-1.el7.x86_64 &> $LOGFILE
-#if [ $? -ne 0 ]; then
+rpm -ql erlang-23.2.6-1.el7.x86_64 &> $LOGFILE
+if [ $? -ne 0 ]; then
     echo -n "Installing $COMPONENT Dependency Package Erlang: "
     yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>> $LOGFILE
     status $?
-#fi
+fi
 echo -n "Configuring $COMPONENT Repository: "
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>> $LOGFILE
 status $?
