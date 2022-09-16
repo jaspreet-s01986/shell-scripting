8d03db87827c29688d3d912249723cc92fb1bbed
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-16 22:12:31 +0530

    commit

--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -11,14 +11,19 @@ status $?
 
 #Calling create user function
 create_user
-echo -n "Downloading $COMPONENT package: "
-cd /home/roboshop
-curl -L -s -o /tmp/payment.zip "https://github.com/stans-robot-project/payment/archive/main.zip"
-status $?
-echo -n "Extracting $COMPONET Package: "
-unzip -o /tmp/payment.zip &>> $LOGFILE
-mv payment-main payment
-status
+#Calling Download & Extract function
+download_extract
+# echo -n "Downloading $COMPONENT package: "
+# cd /home/roboshop
+# curl -L -s -o /tmp/payment.zip "https://github.com/stans-robot-project/payment/archive/main.zip"
+# status $?
+# echo -n "Cleaning up: "
+# cd /home/$APPUSER && rm -rf $COMPONENT &>> $LOGFILE
+# status $?
+# echo -n "Extracting $COMPONET Package: "
+# unzip -o /tmp/payment.zip &>> $LOGFILE
+# mv payment-main payment
+# status $?
 
 echo -n "Installing Dependencies: "
 cd /home/roboshop/payment 
