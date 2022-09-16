f4ecfa9f8187c96966af56e2a66bf3bebc548411
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-16 21:05:21 +0530

    commit

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -7,7 +7,7 @@ source components/common.sh
 rpm -ql erlang-23.2.6-1.el7.x86_64 &> $LOGFILE
 if [ $? -ne 0 ]; then
     echo -n "Installing $COMPONENT Dependency Package Erlang: "
-    yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y
+    yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>> $LOGFILE
     status $?
 fi
 echo -n "Configuring $COMPONENT Repository: "
