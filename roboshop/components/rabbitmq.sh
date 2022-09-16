ce25c3ff39e28c0dc9bca228f1708597d16c8b9b
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-16 20:46:29 +0530

    commit

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -5,7 +5,20 @@ COMPONENT=rabbitmq
 LOGFILE="/tmp/$COMPONENT.log"
 source components/common.sh
 
-maven
+echo -n " Installing $COMPONENT Dependency Package Erlang: "
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y
+status $?
 
+# curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+
+# yum install rabbitmq-server -y
+
+# systemctl enable rabbitmq-server 
+# systemctl start rabbitmq-server
+# systemctl status rabbitmq-server -l
+
+# rabbitmqctl add_user roboshop roboshop123
+# rabbitmqctl set_user_tags roboshop administrator
+# rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
 
 echo -e "\e[32m -------- $COMPONENT Configured Successfully --------\e[0m"
