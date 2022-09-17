0f3397f1150cf67be83d54630672afa542946f8d
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-17 16:49:01 +0530

    commit

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -7,7 +7,7 @@ create_server () {
     #Creating instance & getting private IP in variable
     PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
         sed -e "s/IPADDR/${PRIVATE_IP}/" -e "s/COMPONENT/$COMPONENT/" route53.json > /tmp/route53-record.json
-    echo -ne "Creating Route53 A Record for $COMPONENT:\n"
+    echo -ne "Creating Route53 A Record for \e[35m$COMPONENT\e[0m:\n"
     aws route53 change-resource-record-sets --hosted-zone-id Z08995942J9ZKS0XPUWM5 --change-batch file:///tmp/route53-record.json | jq
 }
 
