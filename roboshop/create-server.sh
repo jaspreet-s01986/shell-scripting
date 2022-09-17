af40b32adaed8167ceab52088f0856e8284de83f
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-17 11:35:23 +0530

    commit

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -2,4 +2,4 @@
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 COMPONENT=$1
 echo "$COMPONENT Server Creation in Progress: "
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
