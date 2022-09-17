edac914a0993d8ddcb85b4fad42f64468ce682c0
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-17 12:17:44 +0530

    commit

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -3,6 +3,8 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-Cen
 COMPONENT=$1
 echo "$COMPONENT Server Creation in Progress: "
 
-PRIVATE_ID=$(aws ec2 run-instancesjjjj --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
-echo $?
-echo $PRIVATE_ID
+PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+sed -e 's/IPADDR/$PRIVATE_IP/' -e 's/COMPONENT=$COMPONENT' route53.json
+
+cat route53.json
