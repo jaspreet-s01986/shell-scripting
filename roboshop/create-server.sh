30018666dc562f849147a8d2b679aa62df494ac4
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-17 12:14:44 +0530

    commit

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -4,5 +4,5 @@ COMPONENT=$1
 echo "$COMPONENT Server Creation in Progress: "
 
 PRIVATE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
-
+echo $?
 echo $PRIVATE_ID
