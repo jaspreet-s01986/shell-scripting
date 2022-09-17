df18c54ae5485a9893b7aaf10afe94ab69ccb7c5
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-17 16:23:51 +0530

    commit

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 COMPONENT=$1
-echo "$COMPONENT Server Creation in Progress: "
+echo -ne "$COMPONENT Server Creation in Progress:\n"
 
 PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-echo -n -e "Component : $COMPONENT\n"
-echo -n -e "Private IP: $PRIVATE_IP\n"
-
 sed -e "s/IPADDR/${PRIVATE_IP}/" -e "s/COMPONENT/$COMPONENT/" route53.json > /tmp/route53-record.json
 
-#echo "Reading route53.json file"
-#cat route53.json
-
-echo "Reading /tmp/route53-record.json file"
-cat /tmp/route53-record.json
+echo -ne "Creating Route53 A Record:\n"
+aws route53 change-resource-record-sets --hosted-zone-id Z08995942J9ZKS0XPUWM5 --change-batch file:///tmp/route53-record.json | jq
