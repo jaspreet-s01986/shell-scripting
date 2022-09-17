3ee8d3c74e0fb94078b68dde7206d9010137e433
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-17 12:48:06 +0530

    commit

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -5,7 +5,7 @@ echo "$COMPONENT Server Creation in Progress: "
 
 PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-sed -e "s/IPADDR/${PRIVATE_IP}/" -e "s/COMPONENT/$COMPONENT/" route53.json >> /tmp/route53-record.json
+sed -e "s/IPADDR/${PRIVATE_IP}/" -e "s/COMPONENT/$COMPONENT/" route53.json > /tmp/route53-record.json
 
 echo "Reading route53.json file"
 cat route53.json
