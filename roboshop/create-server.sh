c5d7d3971e485650af0218a908726a4047545c60
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-17 16:47:51 +0530

    commit

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -3,7 +3,7 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-Cen
 COMPONENT=$1
 
 create_server () {
-    echo -ne "$COMPONENT Server Creation in Progress:\n"
+    echo -ne "\e[35m$COMPONENT\e[0m Server Creation in Progress:\n"
     #Creating instance & getting private IP in variable
     PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
         sed -e "s/IPADDR/${PRIVATE_IP}/" -e "s/COMPONENT/$COMPONENT/" route53.json > /tmp/route53-record.json
