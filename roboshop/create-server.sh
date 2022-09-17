881b9941aca5ccd2468fcb3ff5fe2295cbcdf29f
Jaspreet Singh <jaspreet.s01986@gmail.com>
2022-09-17 10:46:04 +0530

    commit

--- roboshop/create-server.sh
+++ roboshop/create-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId')
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 
 echo $AMI_ID
